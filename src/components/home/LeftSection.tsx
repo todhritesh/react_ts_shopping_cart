15b78ebc4723371e1ba4da35b86256f7e5ecba0b
todhritesh <todhritesh@gmail.com>
2023-02-07 15:44:30 +0530

    added link

--- src/components/home/LeftSection.tsx
+++ src/components/home/LeftSection.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import styles from "./left-section.module.css"
+import { Link } from "react-router-dom";
+
 function LeftSection() {
   return (
     <>
     <div className={`display-2 text-white fw-medium`}>REACT-TYPESCRIPT</div>
     <p className={`display-4 text-white my-3 ${styles.typewriter}`}>CART SYSTEM</p>
     <div>
-        <button className="btn btn-lg btn-outline-light">GET STARTED</button>
+        <Link to="/store" className="btn btn-lg btn-outline-light">GET STARTED</Link>
     </div>
     </>
   )
